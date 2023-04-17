3bec4f7afdce8ab36c91938acc0a1e82b9d4cb9f
Marcos Albano <marcos.albano@idace.ce.gov.br>
2023-04-17 13:46:32 -0300

    DetailOrderService

---
+++ backend/src/services/order/DetailOrderService.ts
@@ -0,0 +1,21 @@
+import prismaClient from "../../prisma";
+
+interface DetailRequest{
+    order_id: string;
+}
+
+class DetailOrderService{
+    async execute({ order_id }: DetailRequest){
+        const orders = prismaClient.item.findMany({
+            where:{
+                order_id: order_id
+            },
+
+        })
+
+        return orders;
+
+    }
+}
+
+export { DetailOrderService }
